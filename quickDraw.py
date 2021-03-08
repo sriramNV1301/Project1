23e3b0960fcc8fa115b37a69b7b75e9cf189f330
Sriram N V <sriramnvilvanathan@gmail.com>
2021-03-08 17:53:19 +0530

    added a partial functio to get verbs

--- quickDraw.py
+++ quickDraw.py
@@ -5,12 +5,24 @@ import SptoText as st
 tag = st.out
 qd = QuickDrawData()
 ext = '.jpg'
+verb = []
 
 for i in tag:
     if i[1] == 'NN' or i[1] == 'NNP':
-        obj= qd.get_drawing(i[0].lower())
-        name = i[0].lower()
-        obj.image.save(name + ext)
-    else: 
+        try:
+            obj= qd.get_drawing(i[0].lower())
+            name = i[0].lower()
+            obj.image.save(name + ext)
+        except:
+            continue
+    
+    elif i[1] == 'VBG':
+        verb.append(i[0])
+    
+    else:
         continue
+
+for v in verb:
+    print(v)
+
 
